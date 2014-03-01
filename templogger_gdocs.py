f2b9a2d431d2cb1a6e1a3532169d3951c7f75e34
gadjetnut <gadjetnut@gmail.com>
2014-03-01 11:59:23 -0500

    Create templogger_gdocs.py

---
+++ templogger_gdocs.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+Rapsberry Pi Projects For Dummies: temperate logger to google docs spreadsheet
+For the Raspberry Pi
+"""
+import subprocess 
+import time
+from ds18b20 import GetTemperature
+from gdocs import LogRowInGDocSpreadsheet
+
+ds18b20_dir     = "28-00000586acff"
+client_id       = '340365379337-f5jjh1t3ao2jv0n4qom599f9bstfbdju.apps.googleusercontent.com'
+client_secret   = 'nZHA3etfvGXVD586FGgWA1lC'
+spreadsheet_key = '0Ah05EaUeJVp0dFIxbWtNb1ZqT2pQa1Z1Nm9RM0U1UFE'
+fahrenheit      = True
+headings = ["Temperature", "Date", "UOM"] #column headings can't have spaces
+
+def main():
+        # This is the main routine of the program
+        
+        # set how long to wait between logs
+        poll_interval = 5
+        
+        if fahrenheit:
+                unit_of_measure = 'F'
+        else:
+                unit_of_measure = 'C'
+                
+        # Get the start time 
+        timer = time.time() - poll_interval
+        
+        # main loop forever
+        while (True):
+                #log the temperature every X seconds
+                if time.time() - timer > poll_interval: 
+                        # get the latest temperature from 
+                        #the sensor
+                        rt = GetTemperature(fahrenheit, ds18b20_dir)
+                        if (rt=="error"):
+                                #reset the timer and try again 
+                                #after the 5 min delay
+                                start_time = time.time()
+                                continue
+                            
+                        temperature=rt
+                        
+                        #Send the temperature reading to Google Docs
+                        LogRowInGDocSpreadsheet(client_id, client_secret, spreadsheet_key, headings, temperature, unit_of_measure);
+                                                
+                        print "Temperature of "+\
+                        str(temperature)+unit_of_measure+" logged"
+                        
+                        # reset the timer
+                        timer = time.time()  
+                time.sleep(.2)
+
+if __name__ == "__main__":
+        main()
