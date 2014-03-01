08d85359fc575f34317ec47e856939f7635ba80c
gadjetnut <gadjetnut@gmail.com>
2014-03-01 18:30:44 -0500

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>An example PHP application</title>
+</head>
+<body>
+    <?php
+    random_number=rand(1,2);
+    if (random_number==1){
+    	echo "Hello world";	
+    }
+    else {
+        echo "I love Raspberry Pi";
+    }
+    ?>
+    </body>
+</html>
